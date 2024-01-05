02abf8a8ec4ae8b5b7fd819bc14f9d66cbac9d92
SagarBhale <94820935+SagarBhale@users.noreply.github.com>
2024-01-05 13:44:51 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -3,9 +3,29 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+  
+    let stack = [];
+    const bracketsArray = {
+      '{':'}',
+      '[':']',
+      '(':')'
+    }
+    for(i=0; i < data[0].length; i++) {
+        if (s[0][i] == '{' || s[0][i] == '(' || s[0][i] == '[') {
+            stack.push(s[0][i]);
+        } else if (s[0][i] == '}' || s[0][i] == ']' || s[0][i] == ')') {
+            if (bracketsArray[stack[stack.length-1]] ==s[0][i]) {
+              stack.pop()
+            }
+        }  
+    }
     
+    if (stack.length == 0) {
+      return true
+    } else {
+      return false
+    }
 };
 
 module.exports = { isValid };
-
 
