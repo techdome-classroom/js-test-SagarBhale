e9df29a6cb8766807a6c31992fbee027de4a82a5
SagarBhale <94820935+SagarBhale@users.noreply.github.com>
2024-01-05 13:50:02 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -2,8 +2,40 @@
  * @param {string} s
  * @return {number}
  */
+
+
 var romanToInt = function(s) {
-    
+    // Define a map for Roman numeral values
+    const romanValues = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
+    let total = 0;
+    let prevValue = 0;
+
+    // Iterate through each character in the string
+    for (let i = s.length - 1; i >= 0; i--) {
+        let currentValue = romanValues[s[i]];
+
+        // If the current value is less than the previous value,
+        // we need to subtract it, otherwise add it
+        if (currentValue < prevValue) {
+            total -= currentValue;
+        } else {
+            total += currentValue;
+        }
+        
+        // Update the previous value for the next iteration
+        prevValue = currentValue;
+    }
+
+    return total;
 };
 
 
